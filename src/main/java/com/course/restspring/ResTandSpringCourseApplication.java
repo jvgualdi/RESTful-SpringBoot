ea9cb2ebbfeb8a13f549da8eb5051f2b61e6c2fe
José Vitor Gualdi <jvgualdi1@gmail.com>
2021-06-12 17:29:51 -0300

    Project Structure

---
+++ src/main/java/com/course/restspring/ResTandSpringCourseApplication.java
@@ -0,0 +1,13 @@
+package com.course.restspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTandSpringCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTandSpringCourseApplication.class, args);
+    }
+
+}
