bf50da2b2b941d0cb8e136421d3d9d0065ac96d0
José Vitor Gualdi <jvgualdi1@gmail.com>
2021-06-25 18:32:56 -0300

    Created a JPARepository for all Entities. To make simple operations on DB through Java code

---
+++ src/main/java/com/course/restspring/pontointeligente/repositories/LancamentoRepository.java
@@ -0,0 +1,24 @@
+package com.course.restspring.pontointeligente.repositories;
+
+import com.course.restspring.pontointeligente.entities.Lancamento;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import java.util.Date;
+import java.util.List;
+
+@Transactional(readOnly = true) //Doesn't make the DB stop, since it is a Read Only Operation
+@NamedQueries({
+    @NamedQuery(name = "LancamentoRepository.findByFuncionarioId",
+                query = "SELECT lanc FROM Lancamento lanc WHERE lanc.funcionario.id = :funcionarioId") })
+
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+    List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
+
+    Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
+}
